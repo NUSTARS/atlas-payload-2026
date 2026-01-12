3539099155555ccc04cdca044f5de53981c73026
Jeongwoo Cheon <70105648+jwoo1127@users.noreply.github.com>
2026-01-12 11:10:53 -0600

    created platformio project for payload

---
+++ Payload-Microcontroller/src/main.cpp
@@ -0,0 +1,20 @@
+// main.cpp
+
+// INCLUDES
+#include <Arduino.h>
+
+// GLOBALS
+
+
+// INTERRUPTS
+
+
+// main
+void setup() {
+  Serial.begin(115200); // Initialize serial communication
+}
+
+void loop() {
+  Serial.println("Hello, World!"); // Print "Hello, World!" to the Serial Monitor
+  delay(1000); // Wait for 1 second
+}
