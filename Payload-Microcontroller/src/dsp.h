cbd3b27f9b3eeeae0e08c26ccd51dc615b8ca634
burkeastanton <burkeastanton@gmail.com>
2026-01-13 19:48:02 -0600

    added eigen + merge

---
+++ Payload-Microcontroller/src/dsp.h
@@ -0,0 +1,10 @@
+// dsp.h
+
+// kalmanFilter: [state] -> [state]
+// does the kalman filter :)
+
+
+
+// getAngularAcceleration: float -> float
+// takes the derivative of the filtered
+// angular velocity signal to 
