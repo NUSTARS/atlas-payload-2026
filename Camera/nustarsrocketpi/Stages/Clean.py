f5e81254e30bbd85b4371acff268de85115feeb6
carterhariss <carterharris410@gmail.com>
2026-01-18 13:46:54 -0600

    Clean.py implemented

--- Camera/nustarsrocketpi/Stages/Clean.py
+++ Camera/nustarsrocketpi/Stages/Clean.py
@@ -1,9 +1,12 @@
 from Stage import Stage
-from PIL.Image import Image   # ✅ import the class, not the module
+from PIL import Image, ImageFilter  # ✅ import the class, not the module
 
 class Clean(Stage):
-    def open(self): pass
-    def close(self): pass
+    def open(self): 
+        self.filter =  ImageFilter.MedianFilter(3)
 
-    def process(self, image: Image) -> Image:
-        pass
+    def close(self): 
+        self.filter = None
+
+    def process(self, image: Image.Image) -> Image.Image:
+        return image.convert("L").filter(self.filter) 
